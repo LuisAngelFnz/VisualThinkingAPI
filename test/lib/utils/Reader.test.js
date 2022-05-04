634566489845006e8a4a0e02b987aa99e06b4bf0
LuisAngelFnz <lfernandez@gmail.com>
2022-05-04 01:13:35 -0500

    Subiendo prueba unitaría 3 y codigo para método loadJson

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -2,16 +2,22 @@ const Reader = require('../../../lib/utils/Reader');
 
 describe('Test Suite for class Reader',() => {
     test('1.- Method loadJson, pass params null and undefined', () => {
-        let callback = () => Reader.loadJson(null);
-        expect(callback).toThrow(TypeError);
+        let callback = () => {Reader.loadJson(null);}
+        expect(callback).toThrow(/Received null/);
 
-        callback = () => eader.loadJson(undefined);
-        expect(callback).toThrow(TypeError);
+        callback = () => {Reader.loadJson(undefined);}
+        expect(callback).toThrow(/Received undefined/);
     });
 
     test('2.- Method loadJson, pass param json OK', () => {
         const result = Reader.loadJson('students.json');
         expect(result).not.toBeUndefined();
         expect(result.length > 0).toBe(true);
+    });
+
+    test('3. Method loadJson, pass path no valid', () => {
+        const callback = () => {Reader.loadJson(`students_${new Date().valueOf()}.json`);}
+        expect(callback).toThrow(/no such file/)
+
     });
 });

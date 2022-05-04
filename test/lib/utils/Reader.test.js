a594ef1543ad385784a6b78d8313cba67306c622
LuisAngelFnz <lfernandez@gmail.com>
2022-05-04 00:40:04 -0500

    Incorporando Suit de pruebas unitarías para clase Reader

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,17 @@
+const Reader = require('../../../lib/utils/Reader');
+
+describe('Test Suite for class Reader',() => {
+    test('1.- Method loadJson, pass params null and undefined', () => {
+        let callback = () => Reader.loadJson(null);
+        expect(callback).toThrow(TypeError);
+
+        callback = () => eader.loadJson(undefined);
+        expect(callback).toThrow(TypeError);
+    });
+
+    test('2.- Method loadJson, pass param json OK', () => {
+        const result = () => Reader.loadJson('students.json');
+        expect(result).not.toBeUndefined();
+        expect(result.length > 0).toBe(true);
+    });
+});

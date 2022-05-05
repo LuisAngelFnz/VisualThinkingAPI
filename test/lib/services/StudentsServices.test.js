34b3653d451ecf62bf8445ce102da5aa45c7f818
LuisAngelFnz <lfernandez@gmail.com>
2022-05-05 11:12:57 -0500

    Redifiniendo test 2 y agregando test 2 de método getMailsByhaveCertification

--- test/lib/services/StudentsServices.test.js
+++ test/lib/services/StudentsServices.test.js
@@ -9,10 +9,35 @@ describe('Suit test for class StudentsServices', () => {
     });
 
     test('2.- Method getMailsByhaveCertification, pass null and undefined params', () => {
-        let callback = () => {StudentsServices.getMailsByhaveCertification(null,null)};
+        let callback = () => {StudentsServices.getMailsByhaveCertification(null)};
         expect(callback).toThrow(TypeError);
 
-        callback = () => {StudentsServices.getMailsByhaveCertification(undefined,undefined)};
+        callback = () => {StudentsServices.getMailsByhaveCertification(undefined)};
         expect(callback).toThrow(TypeError);
     });
+
+    test('3.- Method getMailsByhaveCertification, pass params OK', () => {
+        const students = [
+            {
+                "email": "Todd@visualpartnership.xyz",
+                "haveCertification": true,
+            },
+            {
+                "email": "Sexton@visualpartnership.xyz",
+                "haveCertification": true
+            },
+            {
+                "email": "Sharlene@visualpartnership.xyz",
+                "haveCertification": false
+            }
+        ]
+
+        let result = StudentsServices.getMailsByhaveCertification(students);
+        expect(result).not.toBeUndefined();
+        expect(result.length === 2).toBe(true);
+        expect(result).toContain('Todd@visualpartnership.xyz');
+        expect(result).toContain('Sexton@visualpartnership.xyz');
+    });    
+
+
 });

08be56cf88e4bbdb4cab05e8bbb191588bc1323b
LuisAngelFnz <lfernandez@gmail.com>
2022-05-05 11:40:30 -0500

    Integrando pruebas unitarias 4 y 5 para método filterStudentsByCredits

--- test/lib/services/StudentsServices.test.js
+++ test/lib/services/StudentsServices.test.js
@@ -19,16 +19,16 @@ describe('Suit test for class StudentsServices', () => {
     test('3.- Method getMailsByhaveCertification, pass params OK', () => {
         const students = [
             {
-                "email": "Todd@visualpartnership.xyz",
-                "haveCertification": true,
+                'email': 'Todd@visualpartnership.xyz',
+                'haveCertification': true,
             },
             {
-                "email": "Sexton@visualpartnership.xyz",
-                "haveCertification": true
+                'email': 'Sexton@visualpartnership.xyz',
+                'haveCertification': true
             },
             {
-                "email": "Sharlene@visualpartnership.xyz",
-                "haveCertification": false
+                'email': 'Sharlene@visualpartnership.xyz',
+                'haveCertification': false
             }
         ]
 
@@ -37,7 +37,48 @@ describe('Suit test for class StudentsServices', () => {
         expect(result.length === 2).toBe(true);
         expect(result).toContain('Todd@visualpartnership.xyz');
         expect(result).toContain('Sexton@visualpartnership.xyz');
-    });    
+    });  
 
+    test('4.- Method filterStudentsByCredits, pass params null and undefined', () => {
+        let callback = () => {StudentsServices.filterStudentsByCredits(null, null, null);}
+        expect(callback).toThrow(TypeError);
 
+        callback = () => {StudentsServices.filterStudentsByCredits(undefined, undefined, undefined);}
+        expect(callback).toThrow(TypeError);
+    });
+
+    test('5.- Method filterStudentsByCredits, pass params OK', () => {
+        const students = [
+            {'credits': 400},
+            {'credits': 500},
+            {'credits': 600},
+        ];
+
+        let result =  StudentsServices.filterStudentsByCredits(students, 500, 'e');
+        expect(result).not.toBeUndefined();
+        expect(result.length).not.toBe(1);
+        expect(result[0].credits).toBe(500);
+
+        result =  StudentsServices.filterStudentsByCredits(students, 500, 'gte');
+        expect(result).not.toBeUndefined();
+        expect(result.length).toBe(2);
+        expect(result[0].credits).toBe(500);
+        expect(result[1].credits).toBe(600);
+
+        result =  StudentsServices.filterStudentsByCredits(students, 500, 'gt');
+        expect(result).not.toBeUndefined();
+        expect(result.length).toBe(1);
+        expect(result[0].credits).toBe(600);
+
+        result =  StudentsServices.filterStudentsByCredits(students, 500, 'lte');
+        expect(result).not.toBeUndefined();
+        expect(result.length).toBe(2);
+        expect(result[0].credits).toBe(400);
+        expect(result[1].credits).toBe(500);
+
+        result =  StudentsServices.filterStudentsByCredits(students, 500, 'lt');
+        expect(result).not.toBeUndefined();
+        expect(result.length).toBe(1);
+        expect(result[0].credits).toBe(400);
+    })
 });
